df4a2f474a0a95d10bced36bde8f65d9a75fdd91
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-02-03 16:17:19 +0800

    + 添加Demo

---
+++ servlet-jsp-springmvc-demos/app01c/src/main/java/app01c/SimpleServlet.java
@@ -0,0 +1,21 @@
+package app01c;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SimpleServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 8946L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.print("<html><head></head>" +
+                "<body>Simple Servlet</body></html");
+    }
+}
