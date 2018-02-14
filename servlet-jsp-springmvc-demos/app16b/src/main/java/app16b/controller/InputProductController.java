e99a7ca2864b414e5594762a533928086b075db5
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-02-14 12:44:57 +0800

    + 添加 Demo

---
+++ servlet-jsp-springmvc-demos/app16b/src/main/java/app16b/controller/InputProductController.java
@@ -0,0 +1,10 @@
+package app16b.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InputProductController implements Controller {
+    public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        return "/WEB-INF/jsp/ProductForm.jsp";
+    }
+}
--- servlet-jsp-springmvc-demos/app16b/src/main/java/app16b/servlet/DispatcherServlet.java
+++ servlet-jsp-springmvc-demos/app16b/src/main/java/app16b/servlet/DispatcherServlet.java
@@ -1,5 +1,7 @@
 package app16b.servlet;
 
+import app16b.controller.InputProductController;
+import app16b.controller.SaveProductController;
 import app16b.domain.Product;
 import app16b.form.ProductForm;
 
@@ -11,8 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ControllerServlet", urlPatterns = {"*.action"})
-public class ControllerServlet extends HttpServlet {
+@WebServlet(name = "DispatcherServlet", urlPatterns = {"*.action"})
+public class DispatcherServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 748495L;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
     }
@@ -22,33 +27,18 @@ public class ControllerServlet extends HttpServlet {
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
         String uri = request.getRequestURI();
         int lastIndex = uri.lastIndexOf("/");
         String action = uri.substring(lastIndex + 1);
-        if (action.equals("product_input.action")) {
-            //
-        } else if (action.equals("product_save.action")) {
-            ProductForm productForm = new ProductForm();
-            productForm.setName(request.getParameter("name"));
-            productForm.setDescription(request.getParameter("description"));
-            productForm.setPrice(request.getParameter("price"));
-
-            Product product = new Product();
-            product.setName(productForm.getName());
-            product.setDescription(productForm.getDescription());
-            try {
-                product.setPrice(Float.parseFloat(productForm.getPrice()));
-            } catch (NumberFormatException e) {
-                //
-            }
-            request.setAttribute("product", product);
-        }
 
         String dispatchUrl = null;
         if (action.equals("product_input.action")) {
-            dispatchUrl = "/WEB-INF/jsp/ProductForm.jsp";
+            InputProductController controller = new InputProductController();
+            dispatchUrl = controller.handleRequest(request, response);
         } else if (action.equals("product_save.action")) {
-            dispatchUrl = "/WEB-INF/jsp/ProductDetails.jsp";
+            SaveProductController controller = new SaveProductController();
+            dispatchUrl = controller.handleRequest(request, response);
         }
         if (dispatchUrl != null) {
             RequestDispatcher dispatcher = request.getRequestDispatcher(dispatchUrl);
