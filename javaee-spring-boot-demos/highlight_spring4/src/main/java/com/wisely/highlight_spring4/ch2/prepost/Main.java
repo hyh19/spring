5435bd59fc6f96cb49d961e1fc4cd73874861648
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-02-17 21:05:01 +0800

    + 添加 Demo

---
+++ javaee-spring-boot-demos/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/prepost/Main.java
@@ -0,0 +1,11 @@
+package com.wisely.highlight_spring4.ch2.prepost;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PrePostConfig.class);
+        BeanWayService beanWayService = context.getBean(BeanWayService.class);
+        context.close();
+    }
+}
---
+++ javaee-spring-boot-demos/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/prepost/PrePostConfig.java
@@ -0,0 +1,15 @@
+package com.wisely.highlight_spring4.ch2.prepost;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.wisely.highlight_spring4.ch2.prepost")
+public class PrePostConfig {
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    BeanWayService beanWayService() {
+        return new BeanWayService();
+    }
+}
