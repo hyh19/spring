22b61c3aab3c9dbd98313c2aa00617913696c003
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-02-17 17:03:00 +0800

    + 添加 Demo

---
+++ javaee-spring-boot-demos/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/scope/Main.java
@@ -0,0 +1,16 @@
+package com.wisely.highlight_spring4.ch2.scope;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfig.class);
+        DemoSingletonService s1 = context.getBean(DemoSingletonService.class);
+        DemoSingletonService s2 = context.getBean(DemoSingletonService.class);
+        System.out.println(s1 == s2);
+
+        DemoPrototypeService p1 = context.getBean(DemoPrototypeService.class);
+        DemoPrototypeService p2 = context.getBean(DemoPrototypeService.class);
+        System.out.println(p1 == p2);
+    }
+}
