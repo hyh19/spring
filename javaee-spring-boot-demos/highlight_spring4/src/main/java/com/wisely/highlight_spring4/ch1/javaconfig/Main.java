21ad2b59ab5938a77706813cca76d0523f9c8865
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-02-17 15:16:13 +0800

    + 添加 Demo

--- javaee-spring-boot-demos/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch1/javaconfig/Main.java
+++ javaee-spring-boot-demos/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch1/javaconfig/Main.java
@@ -1,12 +1,13 @@
-package com.wisely.highlight_spring4.ch1.di;
+package com.wisely.highlight_spring4.ch1.javaconfig;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(JavaConfig.class);
         UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
-        System.out.println(useFunctionService.SayHello("world"));
+        System.out.println(useFunctionService.SayHello("java config"));
         context.close();
     }
 }
