47cd81ff61753aa441fdee0ccee971a3517327b1
mrhuangyuhui <mrhuangyuhui@gmail.com>
2018-02-18 16:28:35 +0800

    + 添加 Demo

---
+++ javaee-spring-boot-demos/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch3/annotation/DemoService.java
@@ -0,0 +1,10 @@
+package com.wisely.highlight_spring4.ch3.annotation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public void outputResult() {
+        System.out.println("Hello World");
+    }
+}
