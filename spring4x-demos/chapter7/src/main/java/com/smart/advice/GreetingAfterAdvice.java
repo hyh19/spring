a1a3793e8e5ca31f456a419512d6f4071aefc518
Yuhui Huang <mrhuangyuhui@gmail.com>
2018-03-07 16:12:03 +0800

    + 添加 Demo

---
+++ spring4x-demos/chapter7/src/main/java/com/smart/advice/GreetingAfterAdvice.java
@@ -0,0 +1,11 @@
+package com.smart.advice;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+public class GreetingAfterAdvice implements AfterReturningAdvice {
+    public void afterReturning(Object o, Method method, Object[] objects, Object o1) throws Throwable {
+        System.out.println("Please enjoy yourself!");
+    }
+}
